b463995fefeea8f06be9a3deedd9332c0bf53dfc
Renaldyp <37020760+renaldypermana@users.noreply.github.com>
2021-11-03 23:05:34 +0700

    learn day 1

---
+++ array2d.py
@@ -0,0 +1,22 @@
+# Tetapkan dictionary ke variable fruits
+fruits = {'apel':'apple','jeruk':'orange'}
+
+# Cetak nilai dengan kunci 'jeruk'
+print(fruits['jeruk'])
+
+# Dengan menggunakan dictionary fruits, cetak 'Bahasa Inggris apel adalah ___'
+print('Bahasa Inggris apel adalah ' + fruits['apel'])
+
+
+#update value of array 2d
+fruits = {'apel': 1, 'pisang': 2, 'jeruk': 4}
+
+# Perbarui nilai dengan kunci 'pisang' ke 3
+fruits['pisang'] = 3
+
+# Tambahkan element ke fruits dengan kunci 'anggur' dan nilai 5
+fruits['anggur'] = 5
+
+# Cetak fruits
+print(fruits)
+
